bb6fd8bde64cc67e3c556ab1adaa94eaf1f6d2de
21P31A1243 <99863252+21P31A1243@users.noreply.github.com>
2022-10-12 12:12:47 +0530

    // happy number #include<stdio.h> #include<math.h> int happy(int x) {int sum=0,r,res; while (1>0){     sum=0;     while (x>0){         r=x%10;         sum+=pow(r,2);         x=x/10;
        }if (sum>9){
            x=sum;
            continue;
            
        }else{
            break;
            
        }
        
    }if (sum==1 || sum==7){
        res=1;
        return res;
        
    }else{
        res=0;
        return res;
        
    }
        
    }
    int main(){
        int a,res;
        scanf("%d",&a);
        res=happy(a);
        if (res==1){
            printf("True");
        }else{
            printf("False");
        }
        return 0;
        
    }

--- happy_number.c
+++ happy_number.c
@@ -1,22 +1,44 @@
+// happy number
 #include<stdio.h>
-int main()
-{
- int i,j,n,t,s=0;
- scanf("%d",&n);
-  while(s!=1 && s!=4)
-  {
-   s=0;
-   while(n>0)
-  {
-    j=n%10;
-    s+=(j*j);
-    n=n/10; 
-  }
-  n=s;
-  }
-  
-  if(s==1)
- printf("True");
- else
- printf("False");
+#include<math.h>
+int happy(int x)
+{int sum=0,r,res;
+while (1>0){
+    sum=0;
+    while (x>0){
+        r=x%10;
+        sum+=pow(r,2);
+        x=x/10;
+        
+    }if (sum>9){
+        x=sum;
+        continue;
+        
+    }else{
+        break;
+        
+    }
+    
+}if (sum==1 || sum==7){
+    res=1;
+    return res;
+    
+}else{
+    res=0;
+    return res;
+    
+}
+    
+}
+int main(){
+    int a,res;
+    scanf("%d",&a);
+    res=happy(a);
+    if (res==1){
+        printf("True");
+    }else{
+        printf("False");
+    }
+    return 0;
+    
 }
