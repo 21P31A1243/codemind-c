fddaecad17f0feab9deb4a9b31a0cbac50897b0d
21P31A1243 <99863252+21P31A1243@users.noreply.github.com>
2022-10-16 16:32:44 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0 &&i%2==0)         {            c=1;         }         else if(a[i]%2==0 && i%2!=0)         {             c=0;             break;         }     }     if(c==1)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -1,19 +1,34 @@
 #include<stdio.h>
-int main(){
-    int a[20],i,n;
+int main()
+{
+    int n;
     scanf("%d",&n);
-    for(i=0;i<n;i++){
-            scanf("%d",&a[i]);
-        
+    int a[n],i,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++){
-        if(a[i]%2==0){
-            if(i%2!=0){
-                printf("False");
-                return 0;
-            }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0 &&i%2==0)
+        {
+           c=1;
+        }
+        else if(a[i]%2==0 && i%2!=0)
+        {
+            c=0;
+            break;
         }
     }
-    printf("True");
-    return 0;
+    if(c==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
+ 
+
+ 
